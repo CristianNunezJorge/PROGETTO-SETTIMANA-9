f6db2902070a34eef277a01f233104a0c2bdcacd
CristianNunezJorge <cnunez493@gmail.com>
2026-07-03 17:54:58 +0200

    .

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,14 +1,25 @@
 import { Card } from "react-bootstrap";
+import { useState } from "react";
+import FilmModal from "./FilmModal"
 
 function FilmCard(props) {
+  const [show, setShow] = useState(false);
+
   return (
-    <Card className="bg-transparent border-0">
+    <>
+    <Card
+      className="bg-transparent border-0"
+      onClick={() => setShow(true)}
+      style={{ cursor: "pointer" }}
+    >
       <Card.Img
         variant="top"
         src={props.film.Poster}
         style={{ height: "18rem", objectFit: "cover" }}
       />
     </Card>
+    <FilmModal show={show} onHide={() => setShow(false)} film={props.film} />
+    </>
   );
 }
 
---
+++ src/components/FilmModal.jsx
@@ -0,0 +1,5 @@
+function FilmModal(propos) {
+    return (
+        
+    )
+}
